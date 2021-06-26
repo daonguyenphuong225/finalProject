62f6bc8a88daba494751def393647cc3234b1da9
hoang <viethoang28072000@gmail.com>
2021-06-26 22:34:39 +0700

    fix logic

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "public")));
-app.use(session({ secret: "secret", resave: false, saveUninitialized: true, cookie: { secure: true } }));
+app.use(session({ secret: process.env.privateKey, resave: false, saveUninitialized: true, cookie: { secure: true } }));
 
 app.use("/", Router);
 app.get("/login", (req, res) => {
